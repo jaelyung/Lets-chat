c914d553b0e8eeb3506a2cd8aafa76dd968a1bf4
jaelyung <isiskny0304@gmail.com>
2021-12-24 00:35:28 +0900

    JPA 설정 - @Entity, @Id

--- src/main/java/chatting/chatting/member/domain/Member.java
+++ src/main/java/chatting/chatting/member/domain/Member.java
@@ -1,7 +1,12 @@
 package chatting.chatting.member.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Member {
 
+    @Id
     private String id;
     private String password;
     private String name;
