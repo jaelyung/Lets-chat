4a9fd823fcfa7ebdf055be999ced494e44fc1bfc
jihyun <55981401+cat2998@users.noreply.github.com>
2021-12-28 19:52:37 +0900

    권지현 : MemberMapper 적용

--- src/main/java/chatting/chatting/login/service/LoginService.java
+++ src/main/java/chatting/chatting/login/service/LoginService.java
@@ -1,20 +1,24 @@
 package chatting.chatting.login.service;
 
 import chatting.chatting.member.domain.Member;
+import chatting.chatting.member.mapper.MemberMapper;
 import chatting.chatting.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginService {
 
-    private final MemberRepository memberRepository;
+    //private final MemberRepository memberRepository;
+    @Autowired
+    private MemberMapper memberMapper;
 
-    public LoginService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+//    public LoginService(MemberRepository memberRepository) {
+//        this.memberMapper = memberRepository;
+//    }
 
     public Member login(String loginId, String password) {
-         return memberRepository.findById(loginId)
+         return memberMapper.findById(loginId)
                 .filter(m -> m.getPassword().equals(password))
                 .orElse(null);
     }
