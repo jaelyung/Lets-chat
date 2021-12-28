b1e74ddb3a7af57075c1cabfda2473a5d6ceda9a
jihyun <55981401+cat2998@users.noreply.github.com>
2021-12-28 19:56:22 +0900

    권지현 : 최초생성

---
+++ src/main/java/chatting/chatting/member/mapper/MemberMapper.java
@@ -0,0 +1,32 @@
+package chatting.chatting.member.mapper;
+
+import chatting.chatting.member.domain.Member;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Optional;
+
+@Mapper
+public interface MemberMapper {
+
+    /**
+     * 회원등록
+     */
+    int save(Member member);
+
+    /**
+     * 회원조회 - ID
+     */
+    Optional<Member> findById(String id);
+
+    /**
+     * 회원조회 - NAME
+     */
+    Optional<Member> findByName(String name);
+
+    /**
+     * 전체회원조회
+     */
+    List<Member> findAll();
+
+}
