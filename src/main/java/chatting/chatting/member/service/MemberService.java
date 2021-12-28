7e0ee45564579d8319b667fc65b825f4b7c92e6e
jihyun <55981401+cat2998@users.noreply.github.com>
2021-12-28 19:53:30 +0900

    권지현 : MemberMapper 적용

--- src/main/java/chatting/chatting/member/service/MemberService.java
+++ src/main/java/chatting/chatting/member/service/MemberService.java
@@ -1,33 +1,40 @@
 package chatting.chatting.member.service;
 
 import chatting.chatting.member.domain.Member;
+import chatting.chatting.member.mapper.MemberMapper;
 import chatting.chatting.member.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-@Transactional
+//@Transactional
+@Service
 public class MemberService {
 
-    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository;
+//
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
 
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    @Autowired
+    private MemberMapper memberMapper;
 
     /**
      * 회원가입
      */
-    public String join(Member member) {
+    public int join(Member member) {
 
         validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+        int result = memberMapper.save(member);
+        return result;
     }
 
     private void validateDuplicateMember(Member member) {
-        memberRepository.findById(member.getId())
+        memberMapper.findById(member.getId())
                 .ifPresent(member1 -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
@@ -37,13 +44,13 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        return memberMapper.findAll();
     }
 
     /**
      * 회원 조회 - ID
      */
     public Optional<Member> findOne(String memberId) {
-        return memberRepository.findById(memberId);
+        return memberMapper.findById(memberId);
     }
 }
