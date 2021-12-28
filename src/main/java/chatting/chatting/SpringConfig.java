e6de5ea4159026afcace3f40aa420b82b0bda0c3
jihyun <55981401+cat2998@users.noreply.github.com>
2021-12-28 19:49:05 +0900

    권지현 : 주석처리

--- src/main/java/chatting/chatting/SpringConfig.java
+++ src/main/java/chatting/chatting/SpringConfig.java
@@ -1,33 +1,33 @@
-package chatting.chatting;
-
-import chatting.chatting.member.repository.JpaMemberRepository;
-import chatting.chatting.member.repository.MemberRepository;
-import chatting.chatting.member.repository.MemoryMemberReporsitory;
-import chatting.chatting.member.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
-
-@Configuration
-public class SpringConfig {
-
-    private final EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
+//package chatting.chatting;
+//
+//import chatting.chatting.member.repository.JpaMemberRepository;
+//import chatting.chatting.member.repository.MemberRepository;
+//import chatting.chatting.member.repository.MemoryMemberReporsitory;
+//import chatting.chatting.member.service.MemberService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.persistence.EntityManager;
+//
+//@Configuration
+//public class SpringConfig {
+//
+//    private final EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+//
+//    @Bean
+//    public MemberService memberService() {
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberReporsitory();
-        return new JpaMemberRepository(em);
-    }
-}
+//        //return new JpaMemberRepository(em);
+//    }
+//}
--- src/test/java/chatting/chatting/service/MemberServiceTest.java
+++ src/test/java/chatting/chatting/service/MemberServiceTest.java
@@ -1,65 +1,65 @@
-package chatting.chatting.service;
-
-import chatting.chatting.member.domain.Member;
-import chatting.chatting.member.repository.MemoryMemberReporsitory;
-import chatting.chatting.member.service.MemberService;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
-class MemberServiceTest {
-
-    MemberService memberService;
-    MemoryMemberReporsitory memoryMemberReporsitory;
-
-    @BeforeEach
-    public void beforeEach() {
-        memoryMemberReporsitory = new MemoryMemberReporsitory();
-        memberService = new MemberService(memoryMemberReporsitory);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memoryMemberReporsitory.clearStore();
-    }
-    @Test
-    void join() throws Exception{
-        //given
-        Member member = new Member();
-        member.setId("id1");
-        member.setPassword("id1");
-        member.setName("김재령");
-
-        //when
-        String saveId = memberService.join(member);
-
-        //then
-        Member findMember = memoryMemberReporsitory.findById(saveId).get();
-        assertEquals(member.getName(), findMember.getName());
-    }
-
-    @Test
-    void 중복_회원_예외() throws Exception {
-        //given
-        Member member1 = new Member();
-        member1.setId("id1");
-        member1.setPassword("id1");
-        member1.setName("김재령");
-
-        Member member2 = new Member();
-        member2.setId("id1");
-        member2.setPassword("id1");
-        member2.setName("김재령");
-
-        //when
-        memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> memberService.join(member2)); //예외가 발생해야 한다.
-
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-    }
-
-}
+//package chatting.chatting.service;
+//
+//import chatting.chatting.member.domain.Member;
+//import chatting.chatting.member.repository.MemoryMemberReporsitory;
+//import chatting.chatting.member.service.MemberService;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.Assertions.*;
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class MemberServiceTest {
+//
+//    MemberService memberService;
+//    MemoryMemberReporsitory memoryMemberReporsitory;
+//
+//    @BeforeEach
+//    public void beforeEach() {
+//        memoryMemberReporsitory = new MemoryMemberReporsitory();
+//        memberService = new MemberService(memoryMemberReporsitory);
+//    }
+//
+//    @AfterEach
+//    public void afterEach() {
+//        memoryMemberReporsitory.clearStore();
+//    }
+//    @Test
+//    void join() throws Exception{
+//        //given
+//        Member member = new Member();
+//        member.setId("id1");
+//        member.setPassword("id1");
+//        member.setName("김재령");
+//
+//        //when
+//        String saveId = memberService.join(member);
+//
+//        //then
+//        Member findMember = memoryMemberReporsitory.findById(saveId).get();
+//        assertEquals(member.getName(), findMember.getName());
+//    }
+//
+//    @Test
+//    void 중복_회원_예외() throws Exception {
+//        //given
+//        Member member1 = new Member();
+//        member1.setId("id1");
+//        member1.setPassword("id1");
+//        member1.setName("김재령");
+//
+//        Member member2 = new Member();
+//        member2.setId("id1");
+//        member2.setPassword("id1");
+//        member2.setName("김재령");
+//
+//        //when
+//        memberService.join(member1);
+//        IllegalStateException e = assertThrows(IllegalStateException.class,
+//                () -> memberService.join(member2)); //예외가 발생해야 한다.
+//
+//        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//    }
+//
+//}
