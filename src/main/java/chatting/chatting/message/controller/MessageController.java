db315a633a59c9be959776900266be1bb14ad2e2
jaelyung <isiskny0304@gmail.com>
2022-01-24 00:01:17 +0900

    김재령 : main -> chat Rename

--- src/main/java/chatting/chatting/message/controller/MessageController.java
+++ src/main/java/chatting/chatting/message/controller/MessageController.java
@@ -1,25 +1,28 @@
 package chatting.chatting.message.controller;
 
+import chatting.chatting.message.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class MessageController {
 
-//    private final MainService mainService;
-//
-//    @Autowired
-//    public MainController(MainService mainService) {
-//        this.mainService = mainService;
-//    }
+    private final MessageService messageService;
 
-    @GetMapping(value = "/main/newChatRoom")
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping(value = "/chat/newChatRoom")
     public String createNewChatRoom(Model model) {
 
         model.addAttribute("username", "id1");
-        
-        return "main/newChatRoom";
+
+        return "chat/newChatRoom";
     }
 
 }
