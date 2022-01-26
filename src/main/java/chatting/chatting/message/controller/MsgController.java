e1361badc8b80848d7f578608f422d90a849bdee
jlkim <isiskny0304@gmail.com>
2022-01-26 15:24:00 +0900

    김재령 : 주석제거

--- src/main/java/chatting/chatting/message/controller/MsgController.java
+++ src/main/java/chatting/chatting/message/controller/MsgController.java
@@ -1,35 +1,13 @@
 package chatting.chatting.message.controller;
 
 import chatting.chatting.message.domain.Message;
-import chatting.chatting.message.domain.MessageRoom;
-import chatting.chatting.message.service.MessageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/chat")
 public class MsgController {
-
-//    private final MessageService messageService;
-//
-//    @Autowired
-//    public MsgController(MessageService messageService) {
-//        this.messageService = messageService;
-//    }
-//
-//    @PostMapping
-//    public MessageRoom createRoom(@RequestParam String name) {
-//        return messageService.createRoom(name);
-//    }
-//
-//    @GetMapping
-//    public List<MessageRoom> findAllRoom() {
-//        return messageService.findAllRoom();
-//    }
 
     private final SimpMessageSendingOperations sendingOperations;
 
